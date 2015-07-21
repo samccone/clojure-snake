0d41cf7ed580252dde6ecd67702e2c9cfdbfa6e3
Sam Saccone <samccone@gmail.com>
2015-07-20 23:08:41 -0400

    wip game end state

--- src/clojure_snake/core.clj
+++ src/clojure_snake/core.clj
@@ -1,10 +1,10 @@
 (ns clojure-snake.core
-  (:import (javax.swing JFrame JPanel)))
+  (:import (javax.swing JFrame JPanel)
+           (java.awt.event WindowListener)))
 
 (def snake (atom [[0 1] [0 2] [0 3] [0 4]]))
 (def size 5)
-(def running true)
-
+(def gameOver (atom false))
 
 (defn moveDown [snake]
   (conj (subvec snake 1) [(first (last snake)) (+ (second (last snake)) 1)]))
@@ -13,21 +13,26 @@
   (doall (for [block snake]
       (.fillRect g (* size (first block)) (* size (second block)) size size))))
 
-(do
-  (let [window (JFrame. "snake!")
+(defn tick [drawable]
+  (swap! snake moveDown)
+  (.repaint drawable))
+
+(defn game []
+  (let [window (proxy [JFrame] ["snake!"])
         drawable (proxy [JPanel] []
                    (paintComponent [g]
                      (proxy-super paintComponent g)
                      (paintSnake g @snake)))]
 
+    (.addWindowListener window (proxy [WindowListener] []
+                          (windowClosing [e] (println "all done") (swap! gameOver (fn [v] false)))))
     (.setSize window 200 200)
     (.add window drawable)
     (.setVisible window true)
 
     (loop []
+      (when (not @gameOver)
+        (println "tick")
         (. Thread sleep 1000)
-        (swap! snake moveDown)
-        (.repaint drawable)
-        (recur))))
-
-
+        (tick drawable)
+        (recur)))))
