cef2415cc74b289dee040ba5f9742b77bd9e0b04
Sam Saccone <samccone@gmail.com>
2015-07-23 23:16:15 -0400

    initial key events

--- src/clojure_snake/core.clj
+++ src/clojure_snake/core.clj
@@ -1,6 +1,6 @@
 (ns clojure-snake.core
   (:import (javax.swing JFrame JPanel)
-           (java.awt.event WindowListener)))
+           (java.awt.event WindowListener KeyListener)))
 
 (def snake (atom [[0 1] [0 2] [0 3] [0 4]]))
 (def size 5)
@@ -17,6 +17,9 @@
   (swap! snake move-down)
   (.repaint drawable))
 
+(defn on-window-close [e] (swap! game-over (fn [v] true)))
+(defn on-key-press [e] (println (.getKeyChar e)))
+
 (defn game []
   (let [window (proxy [JFrame] ["snake!"])
         drawable (proxy [JPanel] []
@@ -25,7 +28,12 @@
                      (paint-snake g @snake)))]
 
     (.addWindowListener window (proxy [WindowListener] []
-                                 (windowClosing [e] (println "all done") (swap! game-over (fn [v] true)))))
+                                 (windowDeactivated [e])
+                                 (windowOpened [e])
+                                 (windowActivated [e])
+                                 (windowClosing [e] (on-window-close e))))
+    (.addKeyListener window (proxy [KeyListener] [] (keyPresses [e] (on-key-press e))))
+    (.setFocusable window true)
     (.setSize window 200 200)
     (.add window drawable)
     (.setVisible window true)
